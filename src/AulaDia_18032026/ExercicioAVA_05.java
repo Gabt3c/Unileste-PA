0601bb6a4656722db87f5d0c366ddd340a5692d3
Gabriel <gking3190@gmail.com>
2026-03-18 21:25:29 -0300

    Conclusão do exercício 5

---
+++ src/AulaDia_18032026/ExercicioAVA_05.java
@@ -0,0 +1,23 @@
+package AulaDia_18032026;
+import java.util.Scanner;
+public class ExercicioAVA_05 { public static void main (String[] args) {
+    //base
+    int Base;
+    //expoente
+    int expo;
+    //resultado
+    int result;
+    //Scanner
+    Scanner ler;
+    ler = new Scanner(System.in);
+    //onde a mágica acontece
+    System.out.println("Informe o valor da base: ");
+    Base = ler.nextInt();
+    System.out.println("Informe o valor do expoente: ");
+    expo = ler.nextInt();
+    result = (int) Math.pow(Base, expo);
+    System.out.println("" + Base + " elevado por " +expo + " é igual a: " + result);
+    ler.close();
+}
+
+}
