fe854c37db7210423665a63c310cb84a0dbe4c47
Gabriel <gking3190@gmail.com>
2026-03-27 13:03:31 -0300

    Nova versão melhorada do trabalho de PA, adicionei um round pro salário ter o valor em centavos

--- src/AulaDia_25032026/Trabalho_PA.java
+++ src/AulaDia_25032026/Trabalho_PA.java
@@ -4,27 +4,33 @@ public class Trabalho_PA {public static void main(String[] args) {
 //Scanner
 Scanner ler; ler = new Scanner(System.in);
 //Valores
- int codcargo;  int vlrsal; double calculo;
+ int codcargo;  double vlrsal; double calculo; double calcArr;
 
 //Entrada de Dados
     System.out.println("insira o código do seu cargo: ");
     codcargo = ler.nextInt();
 
     System.out.println("Insira seu salário: ");
-    vlrsal = ler.nextInt();
+    vlrsal = ler.nextFloat();
 
     if (codcargo == 101) {
         calculo = vlrsal + (vlrsal * 0.10);
-        System.out.println("Seu novo salário é: " + calculo);
+        calcArr = Math.round(calculo * 10) / 10.0;
+    
+        System.out.println("Seu novo salário é: " + calcArr);
     }
         
     else if (codcargo == 102) {
         calculo = vlrsal + (vlrsal * 0.20);
-        System.out.println("Seu novo salário é: " + calculo);
+        calcArr = Math.round(calculo * 100) / 10.0;
+
+        System.out.println("Seu novo salário é: " + calcArr);
     }
      else {
         calculo = vlrsal + (vlrsal * 0.40);
-        System.out.println("Seu novo salário é: " + calculo);
+        calcArr = Math.round(calculo * 100) / 10.0;
+
+        System.out.println("Seu novo salário é: " + calcArr);
     }
     ler.close();
 }
