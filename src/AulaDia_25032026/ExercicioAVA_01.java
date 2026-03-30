0f875faf24e56bdf8577b117ac0111741b127f50
Gabriel <gking3190@gmail.com>
2026-03-29 22:34:43 -0300

    Exercício 1 da aula de 25/03/2026 pronto!

---
+++ src/AulaDia_25032026/ExercicioAVA_01.java
@@ -0,0 +1,36 @@
+package AulaDia_25032026;
+
+import java.util.Scanner;
+public class ExercicioAVA_01 {public static void main(String[] args) {
+    //Static variables
+    int A;
+    int B;
+    int MAIOR = Integer.MAX_VALUE;
+    int MENOR = Integer.MIN_VALUE;
+    //Scanner
+    Scanner scanner; scanner = new Scanner(System.in);
+
+    //Entrada de Dados
+    System.out.println("Insira o primeiro valor: ");
+    A = scanner.nextInt();
+    System.out.println("Insira o segundo valor: ");
+    B = scanner.nextInt();
+
+    if (A > B && A != B) {
+     MAIOR = A;
+     MENOR = B;
+     System.out.println("O maior valor é: " +MAIOR);
+     System.out.println("O menor valor é: " +MENOR);
+        } 
+    else if (B > A && A != B) {
+        MAIOR = B;
+        MENOR = A;
+        System.out.println("O maior valor é: " +MAIOR);
+        System.out.println("O menor valor é: " +MENOR);
+        }
+    else {
+        System.out.println("Os valores apresentados são iguais.");
+    }
+    scanner.close();
+    }
+}
