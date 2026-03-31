a5c4553b17743a362294c873c4ce8807a1c67ba6
Gabriel <gking3190@gmail.com>
2026-03-31 14:08:34 -0300

    conclusão da atividade4

--- src/AulaDia_25032026/ExercicioAVA_02.java
+++ src/AulaDia_25032026/ExercicioAVA_02.java
@@ -1,5 +1,20 @@
 package AulaDia_25032026;
+import java.util.Scanner;
+public class ExercicioAVA_02 {public static void main(String[] args) {
+    //Valores
+    int A; int B;
+    int calculo;
+    //Scanner
+    Scanner ler; 
+    ler = new Scanner(System.in);
+    //Entrada de dados
+    System.out.println("Insira o primeiro valor :");
+    A = ler.nextInt();
+    System.out.println("Insira o segundo valor :");
+    B = ler.nextInt();
+    ler.close();
 
-public class ExercicioAVA_02 {
+  //  if (A && B > 0) {}
+}
 
 }
