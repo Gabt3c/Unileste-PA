21145b5d505f22d3aa2fc7925a9b2b3f76824cbb
Gabriel <gking3190@gmail.com>
2026-04-15 19:54:04 -0300

    feat: conclusão do exercício da atividade realizada em sala, introdução a switch case

---
+++ src/AulaDia_15042026/ATIVIDADE_EM_SALA01.java
@@ -0,0 +1,34 @@
+package AulaDia_15042026;
+import java.util.Scanner;
+public class ATIVIDADE_EM_SALA01 {
+    public static void main(String[] args) {
+        //variables
+        String x;
+        //Scanner
+        Scanner sc = new Scanner(System.in);
+        //Data Reading
+        System.out.println("Please insert one of the options ahead: A, B, C, D or E");
+        x = sc.nextLine();
+        //Switch case
+        switch (x) {
+            case "A":
+                System.out.println("Excelente!");
+                break;
+            case "B":
+                System.out.println("Muito bom!");
+                break;
+            case "C":
+                System.out.println("Bom.");
+                break;
+           case "D":
+                System.out.println("Regular.");
+                break;
+            case "E":
+                System.out.println("Insuficiente.");
+                break;
+        }
+        sc.close();
+    }
+}
+//testar a sintaxe moderna depois!
+
