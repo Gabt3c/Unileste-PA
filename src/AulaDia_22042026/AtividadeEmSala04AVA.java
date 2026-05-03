5558611cdd36f7459d6c88477e3f8837c5a496fc
Gabriel <gking3190@gmail.com>
2026-05-03 17:04:35 -0300

    feat: conclusão do exercício 4 da atividade iniciada em sala

--- src/AulaDia_22042026/AtividadeEmSala04AVA.java
+++ src/AulaDia_22042026/AtividadeEmSala04AVA.java
@@ -1,5 +1,25 @@
 package AulaDia_22042026;
-
+import java.util.Scanner;
 public class AtividadeEmSala04AVA {
+    public static void main(String[] args) {
+    //variables
+    int Alunos, idAlunos=0; 
+    double nota=0;
+    Scanner sc = new Scanner (System.in);
+
+        for (Alunos=1; Alunos <=10; Alunos++) {
+            System.out.println("insira a nota do aluno " +Alunos+ ":");
+            nota = sc.nextDouble();
+
+            System.out.println("Insira a idade do aluno: " +Alunos+ ":");
+            idAlunos = sc.nextInt();
+        }
+        if (idAlunos >= 18){
+            System.out.printf("A nota do aluno %d foi: %.2f\n" ,Alunos, nota);
+            }
+        
+        sc.close();
+        
+    }
 
 }
