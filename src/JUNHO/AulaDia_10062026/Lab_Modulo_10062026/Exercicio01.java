f44b3e86453089fc4698bb2ad3263379c403bce2
Gabriel <gking3190@gmail.com>
2026-06-16 12:55:21 -0300

    conclusão exercício 1/10, continuar no desktop

--- src/JUNHO/AulaDia_10062026/Lab_Modulo_10062026/Exercicio01.java
+++ src/JUNHO/AulaDia_10062026/Lab_Modulo_10062026/Exercicio01.java
@@ -1,14 +1,17 @@
 package JUNHO.AulaDia_10062026.Lab_Modulo_10062026;
 import java.util.Scanner;
 public class Exercicio01 {
-    public static void diasAnos(int a){
-        
+    public static int diasVividos(int a){
+        int diasVividos = a*365;
+        return diasVividos;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int id;
-        System.out.println("Digite sua idade: ");
+        System.out.print("Digite sua idade: ");
         id=sc.nextInt();
+        diasVividos(id);
+        System.out.println("Você viveu um total de "+diasVividos(id)+" dias!");
+        sc.close();
     }
-
 }
